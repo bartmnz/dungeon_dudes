7a72de0234a33cb2cc8e92f2d112f576b3a5d62c
bartmnz <bartmnz@gmail.com>
2016-03-10 14:31:39 +0000

    implemented text-based game play

--- dungeons.py
+++ dungeons.py
@@ -1,17 +1,21 @@
 from random import randint
+import os
 
 def main():
-    print('Let\'s play a little game!')
+    print('Let\'s play a little game!\n')
     x = 0
     ourGuy = Hero()
     play = True
     while (play):
         while ( x < 10 ):
             room = Place(ourGuy)
-            print( room.getName())
+            # print( room.getName())
             room.__visitRoom__() ## remove underscores
             x += 1
-        again = input('play again? yes : no')
+            if ( ourGuy.listHealth() == 0):
+                print ('OHH snap you died\n')
+                x = 10
+        again = input('play again? yes : no\n')
         if (again == 'no' or again == 'n'):
             play = False
     
@@ -20,7 +24,7 @@ class Place:
     def __init__(self, hero):
         nouns = ['desert', 'swamp', 'glade', 'sea', 'forrest', 'lair', 'field', 'pit']
         adjitives = ['arcane ', 'southern ', 'boistrous ', 'lovely ', 'last ' ]
-        loot = ['pointy ', 'brilliant ', 'magical ', 'Thor\'s ']
+        loot = ['a pointy ', 'a brilliant ', 'a magical ', 'Thor\'s ']
         loot2 = ['stick', 'poo', 'diamond', 'cheese grater', 'zombified head']
         self.name = 'The ' + adjitives[randint(0,5)] + nouns[randint(0,7)]
         self.monsters = []
@@ -35,6 +39,9 @@ class Place:
             self.monsters[x] = Monster()
     
     def __visitRoom__(self):
+        os.system('clear')
+        print ('Entering ' + self.name + '\n')
+        print ('There are ' + len(self.monsters) + 'monsters in the room')
         monMax = 0
         heroFirst = False
         for monster in self.monsters:
@@ -42,26 +49,40 @@ class Place:
             if (roll > monMax):
                 monMax = roll
         if (randint(1,6) > monMax):
-            ## prompt user to fight or run
-            ## if run --exit
-            heroFirst = True
+            fight = input('They don\'t see you yet. Do you stay and fight? yes : no\n')
+            if (fight == 'yes' or fight == 'y'):
+                heroFirst = True
+            else:
+                os.system('clear')
+                print('Coward\n')
+                return
+        else:
+            print ('They see you first FIGHT!!!\n')
         self.fight(heroFirst)
     
     def fight(self, heroFirst):
         while (self.hero.listHealth() and self.monsters[0].listHealth()):
             if( heroFirst):
                 if (self.hero.rollDice() >= self.monsters[0].rollDice()):
+                    print('You punch him in the face\n')
                     self.monsters[0].getHurt()
             if (self.monsters[0].hero.rollDice() > self.hero.rollDice()):
+                    print('He punches you in the face\n')
                     self.hero.getHurt()
                     heroFirst = True
             if ( self.monsters[0].listHealth() == 0):
+                print('You killed the monster\n')
                 self.monsters.remove(self.monsters[0])
+                x = len(self.monsters)
+                if ( x > 0):
+                    os.system('clear')
+                    input('There are still ' + x + 'monsters in the room\n')
         if (self.hero.listHealth()):
             self.hero.getLoot(self.treasure)
+            input (' you killed all the monsters and you found ' + self.treasure + '\n')
             
-    def getName(self):
-        return self.name
+    # def getName(self):
+    #     return self.name
 
 
 
