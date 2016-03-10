d5ad771ca747e808187aa91813c13e02d008b731
bartmnz <bartmnz@gmail.com>
2016-03-10 15:12:35 +0000

    fixed runtime errors

--- dungeons.py
+++ dungeons.py
@@ -1,24 +1,7 @@
+#!usr/bin/env python3
 from random import randint
 import os
 
-def main():
-    print('Let\'s play a little game!\n')
-    x = 0
-    ourGuy = Hero()
-    play = True
-    while (play):
-        while ( x < 10 ):
-            room = Place(ourGuy)
-            # print( room.getName())
-            room.__visitRoom__() ## remove underscores
-            x += 1
-            if ( ourGuy.listHealth() == 0):
-                print ('OHH snap you died\n')
-                x = 10
-        again = input('play again? yes : no\n')
-        if (again == 'no' or again == 'n'):
-            play = False
-    
 
 class Place:
     def __init__(self, hero):
@@ -26,7 +9,7 @@ class Place:
         adjitives = ['arcane ', 'southern ', 'boistrous ', 'lovely ', 'last ' ]
         loot = ['a pointy ', 'a brilliant ', 'a magical ', 'Thor\'s ']
         loot2 = ['stick', 'poo', 'diamond', 'cheese grater', 'zombified head']
-        self.name = 'The ' + adjitives[randint(0,5)] + nouns[randint(0,7)]
+        self.name = 'The ' + adjitives[randint(0,4)] + nouns[randint(0,6)]
         self.monsters = []
         self.hero = hero
         self.treasure = loot[randint(0,3)] + loot2[randint(0,4)]
@@ -36,12 +19,16 @@ class Place:
             y = 1 + x % 2
         x = 0 
         while (x < y):
-            self.monsters[x] = Monster()
+            self.monsters.append(Monster())
+            x += 1
     
     def __visitRoom__(self):
         os.system('clear')
         print ('Entering ' + self.name + '\n')
-        print ('There are ' + len(self.monsters) + 'monsters in the room')
+        num = str(len(self.monsters))
+        print ('There are ' + num + ' monsters in the room')
+        if ( num == 0):
+            return
         monMax = 0
         heroFirst = False
         for monster in self.monsters:
@@ -61,12 +48,12 @@ class Place:
         self.fight(heroFirst)
     
     def fight(self, heroFirst):
-        while (self.hero.listHealth() and self.monsters[0].listHealth()):
+        while (self.hero.listHealth() and len(self.monsters)):
             if( heroFirst):
                 if (self.hero.rollDice() >= self.monsters[0].rollDice()):
                     print('You punch him in the face\n')
                     self.monsters[0].getHurt()
-            if (self.monsters[0].hero.rollDice() > self.hero.rollDice()):
+            if (self.monsters[0].rollDice() > self.hero.rollDice()):
                     print('He punches you in the face\n')
                     self.hero.getHurt()
                     heroFirst = True
@@ -75,8 +62,8 @@ class Place:
                 self.monsters.remove(self.monsters[0])
                 x = len(self.monsters)
                 if ( x > 0):
+                    input('There are still ' + str(x) + ' monsters in the room\n')
                     os.system('clear')
-                    input('There are still ' + x + 'monsters in the room\n')
         if (self.hero.listHealth()):
             self.hero.getLoot(self.treasure)
             input (' you killed all the monsters and you found ' + self.treasure + '\n')
@@ -133,3 +120,29 @@ class Hero:
             if( y > max):
                 max = y
         return max
+        
+        
+
+def main():
+    print('Let\'s play a little game!\n')
+    x = 0
+    
+    play = True
+    while (play):
+        ourGuy = Hero()
+        while ( x < 10 ):
+            room = Place(ourGuy)
+            # print( room.getName())
+            room.__visitRoom__() ## remove underscores
+            x += 1
+            if ( ourGuy.listHealth() == 0):
+                print ('OHH snap you died\n')
+                x = 10
+        again = input('play again? yes : no\n')
+        if (again == 'no' or again == 'n'):
+            play = False
+        else:
+            x = 0
+
+if __name__ == "__main__":
+    main()
